51dd6201d67e2039de9eeb1560bc4cbe32e959bc
haykam821 <24855774+haykam821@users.noreply.github.com>
2020-05-09 16:27:57 -0400

    Improve mixin compatibility

--- src/main/java/io/github/haykam821/pling/mixin/SplashScreenMixin.java
+++ src/main/java/io/github/haykam821/pling/mixin/SplashScreenMixin.java
@@ -1,13 +1,14 @@
 package io.github.haykam821.pling.mixin;
 
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import io.github.haykam821.pling.config.ModConfig;
 import me.sargunvohra.mcmods.autoconfig1u.AutoConfig;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.SplashScreen;
 import net.minecraft.client.sound.PositionedSoundInstance;
 import net.minecraft.sound.SoundEvent;
@@ -16,10 +17,11 @@ import net.minecraft.util.registry.Registry;
 
 @Mixin(SplashScreen.class)
 public class SplashScreenMixin {
-	ModConfig config = AutoConfig.getConfigHolder(ModConfig.class).getConfig();
+	@Unique
+	private static ModConfig config = AutoConfig.getConfigHolder(ModConfig.class).getConfig();
 
-	@Redirect(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screen/Screen;init(Lnet/minecraft/client/MinecraftClient;II)V"))
-	public void plingOnResourceReloadDone(Screen screen, MinecraftClient client, int width, int height) {
+	@Inject(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screen/Screen;init(Lnet/minecraft/client/MinecraftClient;II)V"))
+	public void plingWhenResourcesReloaded(CallbackInfo ci) {
 		Identifier soundId = new Identifier(config.sound);
 		SoundEvent soundEvent = Registry.SOUND_EVENT.get(soundId);
 	
@@ -27,7 +29,5 @@ public class SplashScreenMixin {
 			PositionedSoundInstance sound = PositionedSoundInstance.master(soundEvent, config.pitch);
 			MinecraftClient.getInstance().getSoundManager().play(sound);
 		}
-
-		screen.init(client, width, height);
 	}
 }
