71448dad88fba451f332fc970f34e8561d308cef
haykam821 <24855774+haykam821@users.noreply.github.com>
2020-07-03 02:31:00 -0400

    Fix crash when an invalid sound event ID is specified

--- src/main/java/io/github/haykam821/pling/Pling.java
+++ src/main/java/io/github/haykam821/pling/Pling.java
@@ -1,13 +1,37 @@
 package io.github.haykam821.pling;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import io.github.haykam821.pling.config.ModConfig;
 import me.sargunvohra.mcmods.autoconfig1u.AutoConfig;
 import me.sargunvohra.mcmods.autoconfig1u.serializer.GsonConfigSerializer;
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.sound.SoundEvent;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.InvalidIdentifierException;
+import net.minecraft.util.registry.Registry;
 
 public class Pling implements ModInitializer {
+	private static final Logger LOGGER = LogManager.getLogger("Pling");
+	private static final ModConfig CONFIG = AutoConfig.register(ModConfig.class, GsonConfigSerializer::new).getConfig();
+
 	@Override
 	public void onInitialize() {
-		AutoConfig.register(ModConfig.class, GsonConfigSerializer::new);
+		return;
+	}
+
+	public static SoundEvent getLoadingSound() {
+		try {
+			Identifier soundId = new Identifier(CONFIG.sound);
+			return Registry.SOUND_EVENT.get(soundId);
+		} catch (InvalidIdentifierException exception) {
+			LOGGER.warn("Invalid loading sound event ID: '{}'", CONFIG.sound);
+			return null;
+		}
+	}
+	
+	public static ModConfig getConfig() {
+		return CONFIG;
 	}
 }
--- src/main/java/io/github/haykam821/pling/mixin/SplashScreenMixin.java
+++ src/main/java/io/github/haykam821/pling/mixin/SplashScreenMixin.java
@@ -1,32 +1,23 @@
 package io.github.haykam821.pling.mixin;
 
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import io.github.haykam821.pling.config.ModConfig;
-import me.sargunvohra.mcmods.autoconfig1u.AutoConfig;
+import io.github.haykam821.pling.Pling;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.SplashScreen;
 import net.minecraft.client.sound.PositionedSoundInstance;
 import net.minecraft.sound.SoundEvent;
-import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
 
 @Mixin(SplashScreen.class)
 public class SplashScreenMixin {
-	@Unique
-	private static ModConfig config = AutoConfig.getConfigHolder(ModConfig.class).getConfig();
-
 	@Inject(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screen/Screen;init(Lnet/minecraft/client/MinecraftClient;II)V"))
 	public void plingWhenResourcesReloaded(CallbackInfo ci) {
-		Identifier soundId = new Identifier(config.sound);
-		SoundEvent soundEvent = Registry.SOUND_EVENT.get(soundId);
-	
+		SoundEvent soundEvent = Pling.getLoadingSound();
 		if (soundEvent != null) {
-			PositionedSoundInstance sound = PositionedSoundInstance.master(soundEvent, config.pitch);
+			PositionedSoundInstance sound = PositionedSoundInstance.master(soundEvent, Pling.getConfig().pitch);
 			MinecraftClient.getInstance().getSoundManager().play(sound);
 		}
 	}
