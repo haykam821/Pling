42926730b61e6a10b44162ef700c1af237fe6e8d
haykam821 <24855774+haykam821@users.noreply.github.com>
2021-07-08 08:32:40 -0400

    Allow configuring the mod to not play the loading sound when resources are reloaded

--- src/main/java/io/github/haykam821/pling/mixin/SplashScreenMixin.java
+++ src/main/java/io/github/haykam821/pling/mixin/SplashScreenMixin.java
@@ -1,6 +1,8 @@
 package io.github.haykam821.pling.mixin;
 
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -10,8 +12,14 @@ import net.minecraft.client.gui.screen.SplashScreen;
 
 @Mixin(SplashScreen.class)
 public class SplashScreenMixin {
+	@Shadow
+	@Final
+	private boolean reloading;
+
 	@Inject(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screen/Screen;init(Lnet/minecraft/client/MinecraftClient;II)V"))
 	public void plingWhenResourcesReloaded(CallbackInfo ci) {
-		Pling.playEffects();
+		if (!this.reloading || Pling.getConfig().playWhenResourcesReloaded) {
+			Pling.playEffects();
+		}
 	}
 }
